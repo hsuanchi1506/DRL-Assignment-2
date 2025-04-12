d84806df9180f32e762db509813da87f1f84941b
hsuanchi1506 <hsuanchi1506@gmail.com>
2025-04-13 02:50:33 +0800

    trash

--- student_agent.py
+++ student_agent.py
@@ -117,7 +117,13 @@ def get_action(env, score):
     #         # if i > 100 and distribution[best_action] > 0.8:
     #         #     break
 
-    if score > 70000:
+    if score > 100000:
+        for i in range(900):
+            td_mcts.run_simulation(root)
+            best_action, distribution = td_mcts.best_action_distribution(root)
+            if i > 100 and distribution[best_action] > 0.8:
+                break
+    elif score > 70000:
         for i in range(700):
             td_mcts.run_simulation(root)
             best_action, distribution = td_mcts.best_action_distribution(root)
@@ -132,7 +138,7 @@ def get_action(env, score):
                 break
 
 
-    elif score > 30000:
+    elif score > 35000:
         for i in range(200):
             td_mcts.run_simulation(root)
             best_action, distribution = td_mcts.best_action_distribution(root)
