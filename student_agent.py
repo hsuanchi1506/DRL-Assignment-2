24f35fdf6310d4a7660c9a34269e029d3bcb8f04
hsuanchi1506 <hsuanchi1506@gmail.com>
2025-04-12 22:08:28 +0800

    trash

--- student_agent.py
+++ student_agent.py
@@ -118,7 +118,7 @@ def get_action(env, score):
     #         #     break
 
     if score > 50000:
-        for i in range(1000):
+        for i in range(750):
             td_mcts.run_simulation(root)
             best_action, distribution = td_mcts.best_action_distribution(root)
     elif score > 30000:
